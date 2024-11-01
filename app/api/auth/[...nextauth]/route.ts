3357695da364e82c5f0515ac925ed8a933afa010
SaptatirthaChoudhury <saptatirtha@gmail.com>
2024-11-01 20:43:05 +0530

    Write Google/auth public/private key pair useSession export/dashboard write prisma/client in a singleton pattern design prisma/postgres schema and finally migrations

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import db from "@/app/db";
+import { Keypair } from "@solana/web3.js";
+
 // Signup / Signin with google
 const handler = NextAuth({
   providers: [
@@ -8,6 +11,53 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (account?.provider === "google") {
+        const email = user.email;
+        if (!email) {
+          return false;
+        }
+
+        const userDB = await db.user.findFirst({
+          where: {
+            username: email,
+          },
+        });
+
+        if (userDB) {
+          return true;
+        }
+
+        const keypair = Keypair.generate();
+        const publicKey = keypair.publicKey.toBase58();
+        const privateKey = keypair.secretKey;
+        console.log(publicKey);
+        console.log(privateKey);
+
+        await db.user.create({
+          data: {
+            username: email,
+            provider: "Google",
+            solWallet: {
+              create: {
+                publicKey: publicKey,
+                privateKey: privateKey.toString(),
+              },
+            },
+            inrWallet: {
+              create: {
+                balance: 0,
+              },
+            },
+          },
+        })
+
+        return true
+      }
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { SecondaryButton } from "./Button";
+import { useRouter } from "next/navigation";
 
 export const Hero = () => {
+  const session = useSession();
+  const router = useRouter();
   return (
     <div>
       <div className="text-6xl font-medium">
@@ -13,15 +16,29 @@ export const Hero = () => {
       </div>
 
       <div className="flex justify-center pt-4 text-2xl text-slate-500">
-      Create a frictionless wallet from India with just a Google Account.
+        Create a frictionless wallet from India with just a Google Account.
       </div>
       <div className="flex justify-center pt-4 text-2xl text-slate-500">
-      Convert your INR into Cryptocurrency
+        Convert your INR into Cryptocurrency
       </div>
       <div className="pt-8 flex justify-center">
-        <SecondaryButton onClick={() => {
-            signIn("google")
-        }}>Login with Google</SecondaryButton>
+        {session.data?.user ? (
+          <SecondaryButton
+            onClick={() => {
+              router.push("/dashboard");
+            }}
+          >
+            Go to dashboard
+          </SecondaryButton>
+        ) : (
+          <SecondaryButton
+            onClick={() => {
+              signIn("google");
+            }}
+          >
+            Login with Google
+          </SecondaryButton>
+        )}
       </div>
     </div>
   );
